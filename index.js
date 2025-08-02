2861e44767772a723c71d2df8aaaa21e3a0d462e
khanhnam1522 <khanhnam1522@gmail.com>
2025-08-02 15:03:57 -0700

    feat: handle bot exit

--- index.js
+++ index.js
@@ -572,4 +572,25 @@ async function playSong(guildId, song) {
   }
 }
 
+client.on("voiceStateUpdate", (oldState, newState) => {
+  if (oldState.member.user.id !== client.user.id) return;
+
+  if (oldState.channelId && !newState.channelId) {
+    const serverQueue = queue.get(oldState.guild.id);
+    if (serverQueue) {
+      if (serverQueue.nowPlayingMessage) {
+        serverQueue.nowPlayingMessage.delete().catch((err) => {
+          if (err.code !== 10008) {
+            console.error(
+              "Failed to delete 'Now Playing' message on disconnect:",
+              err
+            );
+          }
+        });
+      }
+      queue.delete(oldState.guild.id);
+    }
+  }
+});
+
 client.login(process.env.DISCORD_TOKEN);
